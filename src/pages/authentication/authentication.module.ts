10668e6f93f231802d60b665390f19664cb46b5a
linz <phoneticem@gmail.com>
2022-01-15 13:21:56 +0330

    init

---
+++ src/pages/authentication/authentication.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthenticationComponent } from './authentication.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+
+
+@NgModule({
+  declarations: [
+    AuthenticationComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthenticationModule { }
